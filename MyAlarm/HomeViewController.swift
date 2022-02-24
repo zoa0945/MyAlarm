208fcac194c4af8c76e988d99b48ccd1322a3c4c
Mac <zoa0945@gmail.com>
2022-02-24 20:38:17 +0900

    feat: add UI with storyboard

---
+++ MyAlarm/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  MyAlarm
+//
+//  Created by Mac on 2022/02/24.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension HomeViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+}
---
+++ MyAlarm/NewAlarmViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewAlarmViewController.swift
+//  MyAlarm
+//
+//  Created by Mac on 2022/02/24.
+//
+
+import UIKit
+
+class NewAlarmViewController: UIViewController {
+    
+    @IBOutlet weak var alarmCasePickerView: UIPickerView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapDateButton(_ sender: UIButton) {
+    }
+}
